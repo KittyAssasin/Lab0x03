df152e236402192b61b666e5bd7b8a47cbcb42dd
Zachary Steffen <zdsteffen@gmail.com>
2020-10-03 22:32:38 -0500

    First implementation of 3sum brute

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,6 +2,7 @@ package com.company;
 
 import java.lang.management.ManagementFactory;
 import java.lang.management.ThreadMXBean;
+import java.lang.reflect.MalformedParametersException;
 
 public class Main {
 
@@ -18,20 +19,33 @@ public class Main {
 
         int[] testArray = {-19, -14, -7, -2, 4, 6, 7, 8, 23, 45};
 
+        System.out.println("3sum brute test: " + threeSumBrute(testArray));
         // write your code here
     }
 
     //3sum problem: are there three numbers in this list that sum to 0?
     //3sum brute force (N^3)
-    private static boolean threeSumBrute(int[] array) {}
+    private static boolean threeSumBrute(int[] array) {
+        int len = array.length;
+
+        for (int i = 0; i < len; i++) {
+            for (int k = i+1; k < len; k++) {
+                for (int m = k+1; m < len; m++) {
+                    if ((array[i] + array[k] + array[m]) == 0)
+                        return true;
+                }
+            }
+        }
+        return false;
+    }
 
     //3sum faster (N^2 * log(N))
-    private static boolean threeSumFast() {}
+    //private static boolean threeSumFast(int[] array) {}
 
     //3sum fastest (N^2)
-    private static boolean threeSumFastest() {}
+    //private static boolean threeSumFastest(int[] array) {}
 
-    private static int[] generateArray(int count, int min, int max) {}
+    //private static int[] generateArray(int count, int min, int max) {}
 
     public static long getCpuTime() {
         ThreadMXBean bean = ManagementFactory.getThreadMXBean();
